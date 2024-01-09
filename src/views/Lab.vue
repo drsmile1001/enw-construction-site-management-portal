754445ab735511023804b7778aebe97088579d73
drsmile.lai <drsmile.lai@ennowell.com>
2024-01-09 08:51:41 +0800

    release 0.1.8

--- src/views/Lab.vue
+++ src/views/Lab.vue
@@ -1,3 +1,12 @@
-<template></template>
+<template>
+  {{ counter }}
+  <button @click="plusOne">+1</button>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const counter = ref(0)
+
+function plusOne() {
+  counter.value++
+}
+</script>
