cf7008d4e27da6c84d476f642292ff77d63aa425
drsmile.lai <drsmile.lai@ennowell.com>
2024-01-26 11:48:58 +0800

    feat: header 改深主色底

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,29 +1,33 @@
 <template>
-  <NLayoutHeader
-    class="h-16 px-8 py-4 flex items-center justify-between"
-    bordered
-  >
-    <NBreadcrumb>
-      <NBreadcrumbItem
-        v-for="item in controller.breadcrumbItems"
-        :key="item.label"
-      >
-        <RouterLink
-          class="flex gap-2"
-          :to="{
-            name: item.toRouteName,
-          }"
+  <NConfigProvider :theme="darkTheme">
+    <NLayoutHeader
+      class="h-16 bg-primary-600 px-8 py-4 flex items-center justify-between"
+      bordered
+    >
+      <NBreadcrumb>
+        <NBreadcrumbItem
+          v-for="item in controller.breadcrumbItems"
+          :key="item.label"
         >
-          <span>{{ item.label }}</span>
-          <span v-if="item.name">{{ toValue(item.name) }}</span>
-        </RouterLink>
-      </NBreadcrumbItem>
-    </NBreadcrumb>
-  </NLayoutHeader>
+          <RouterLink
+            class="flex gap-2"
+            :to="{
+              name: item.toRouteName,
+            }"
+          >
+            <span>{{ item.label }}</span>
+            <span v-if="item.name">{{ toValue(item.name) }}</span>
+          </RouterLink>
+        </NBreadcrumbItem>
+      </NBreadcrumb>
+    </NLayoutHeader>
+  </NConfigProvider>
 </template>
 <script setup lang="ts">
+import { NConfigProvider } from "naive-ui"
 import { useLayoutController } from "./LayoutController"
 import { toValue } from "vue"
+import { darkTheme } from "naive-ui"
 
 const controller = useLayoutController()
 </script>
